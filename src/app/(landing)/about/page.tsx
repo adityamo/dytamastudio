d9faa7b25b4cac3cc9e0c01f932633665fc79450
aditya.septama <rfadityahalim18@gmail.com>
2025-08-22 00:13:41 +0700

    add_profile

--- src/app/(landing)/about/page.tsx
+++ src/app/(landing)/about/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroAbout from "@/modules/about/HeroAbout";
 import CarrierHistory from "@/modules/about/CarrierHistory";
+import TechnologyMe from "@/modules/about/TechnologyMe";
 
 const page = () => {
   return (
     <>
       <HeroAbout />
+      <TechnologyMe />
       <CarrierHistory />
     </>
   );
--- src/modules/about/CarrierHistory.tsx
+++ src/modules/about/CarrierHistory.tsx
@@ -1,76 +1,61 @@
 import React from "react";
 import Container from "@/components/container";
+import carrierHistory from "@/data/carrier.json";
 
 const CarrierHistory = () => {
   return (
-    <section className="mt-14  lg:mt-28 relative w-full  bg-gradient-to-t from-white  to-white dark:from-slate-900 dark:to-slate-800">
+    <section className="pt-14  lg:pt-28 relative w-full  bg-white dark:bg-slate-800">
       <Container>
-        <div className="relative">
-          <div className="flex flex-col space-y-4">
-            <p className="text-primary text-xs lg:text-sm font-medium">
-              MY RESUME
-            </p>
-            <h4 className="text-black text-xl lg:text-4xl font-bold ">
-              Tentang Karier saya
-            </h4>
+        <div className="relative w-full">
+          <div className="grid grid-cols-1 lg:grid-cols-5 gap-4">
+            <div className="flex flex-col space-y-4 lg:col-span-2">
+              <p className="text-primary text-xs lg:text-sm font-medium">
+                EXPERIENCE
+              </p>
+              <h4 className="text-black text-xl lg:text-4xl font-bold dark:text-white ">
+                Tentang Karier saya
+              </h4>
+              <p className="text-gray-500 dark:text-white text-sm font-normal">
+                ini adalah beberapa pengalaman saya selama berkarir dibidang IT
+              </p>
+            </div>
+            <div className="lg:col-span-3">
+              <ol className="relative space-y-8 before:absolute before:-ml-px before:h-full before:w-0.5 before:rounded-full before:bg-gray-200">
+                {carrierHistory.map((item, key: React.Key) => {
+                  return (
+                    <li
+                      className="relative -ms-1.5 flex items-start gap-4"
+                      key={key}
+                    >
+                      <span className="size-3 shrink-0 rounded-full bg-primary"></span>
+                      <div className="-mt-2 space-y-5">
+                        <div className="flex w-full justify-between">
+                          <div className="flex flex-col space-y-2">
+                            <h3 className="text-xl font-bold text-black dark:text-secondary">
+                              {item.job_position}
+                            </h3>
+                            <h6 className="text-sm font-semibold text-black dark:text-white">
+                              {item.company}
+                            </h6>
+                          </div>
+
+                          <div>
+                            <time className="text-xs font-medium  px-5 py-2 bg-primary mt-4 mb-4 rounded-full text-white">
+                              {item.start_date} - {item.end_date}
+                            </time>
+                          </div>
+                        </div>
+
+                        <p className="mt-2.5 text-sm text-gray-500 dark:text-white">
+                          {item.job_desc}
+                        </p>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ol>
+            </div>
           </div>
-          <ol className="relative space-y-8 before:absolute before:-ml-px before:h-full before:w-0.5 before:rounded-full before:bg-gray-200">
-            <li className="relative -ms-1.5 flex items-start gap-4">
-              <span className="size-3 shrink-0 rounded-full bg-blue-600"></span>
-
-              <div className="-mt-2">
-                <time className="text-xs/none font-medium text-gray-700">
-                  12/02/2025
-                </time>
-
-                <h3 className="text-lg font-bold text-gray-900">Kickoff</h3>
-
-                <p className="mt-0.5 text-sm text-gray-700">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-                  officiis tempora ipsum adipisci tenetur sunt quae
-                  exercitationem sed pariatur porro!
-                </p>
-              </div>
-            </li>
-
-            <li className="relative -ms-1.5 flex items-start gap-4">
-              <span className="size-3 shrink-0 rounded-full bg-blue-600"></span>
-
-              <div className="-mt-2">
-                <time className="text-xs/none font-medium text-gray-700">
-                  15/03/2025
-                </time>
-
-                <h3 className="text-lg font-bold text-gray-900">
-                  First Milestone
-                </h3>
-
-                <p className="mt-0.5 text-sm text-gray-700">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-                  officiis tempora ipsum adipisci tenetur sunt quae
-                  exercitationem sed pariatur porro!
-                </p>
-              </div>
-            </li>
-
-            <li className="relative -ms-1.5 flex items-start gap-4">
-              <span className="size-3 shrink-0 rounded-full bg-blue-600"></span>
-
-              <div className="-mt-2">
-                <time className="text-xs/none font-medium text-gray-700">
-                  24/04/2025
-                </time>
-
-                <h3 className="text-lg font-bold text-gray-900">Launch</h3>
-
-                <p className="mt-0.5 text-sm text-gray-700">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-                  officiis tempora ipsum adipisci tenetur sunt quae
-                  exercitationem sed pariatur porro!
-                </p>
-              </div>
-            </li>
-          </ol>
         </div>
       </Container>
     </section>
